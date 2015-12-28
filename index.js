4400b2202bd52c8e3613a1a259fad4867532fda3
ArchanaS <archana.june@gmail.com>
2015-12-28 11:52:53 -0800

    Fixed code review comments, added unit tests

--- index.js
+++ index.js
@@ -18,8 +18,7 @@ var appendBoxProps = function(decl, rule) {
   // Identify whether row or column
   var direction = decl.value.indexOf('horizontal') >= 0 ? 'horizontal':'vertical';
 
-  // Remove any formatting on the value
-  var allValues = decl.value.replace(/\'|\"/g, "").split(' ');
+  var allValues = decl.value.split(' ');
 
   // Loop through all values and apply the styles
   for (var i=0; i<allValues.length; i++) {
@@ -35,8 +34,7 @@ var appendBoxProps = function(decl, rule) {
 };
 
 var appendBoxItemProps = function(decl, rule) {
-  // Clean up
-  var allValues = decl.value.replace(/\'|\"/g, "").split(' ');
+  var allValues = decl.value.split(' ');
 
   // For every value, apply styles
   for (var i=0; i<allValues.length; i++) {
@@ -52,24 +50,23 @@ var appendBoxItemProps = function(decl, rule) {
   rule.removeChild(decl);
 };
 
-module.exports = postcss.plugin('flexbox',
+module.exports = postcss.plugin('postcss-flexbox',
   function flexboxAdder(options) {
     return function (css) {
       options = options || {};
 
-      css.walkRules(function (rule) {
-        rule.walkDecls(function (decl, i) {
-          var value = decl.prop;
+      css.walkDecls(function (decl, i) {
+        var rule = decl.parent;
+        var value = decl.prop;
 
-          // Box layout
-          if (decl.prop === 'box') {
-            appendBoxProps(decl, rule);
-          }
-          // Items inside box layout
-          if (decl.prop === 'box-item') {
-            appendBoxItemProps(decl, rule);
-          }
-        });
+        // Box layout
+        if (decl.prop === 'box') {
+          appendBoxProps(decl, rule);
+        }
+        // Items inside box layout
+        if (decl.prop === 'box-item') {
+          appendBoxItemProps(decl, rule);
+        }
       });
     }
   });
---
+++ test.js
@@ -0,0 +1,23 @@
+var postcss = require('postcss');
+var test = require('ava');
+var flexbox = require('./');
+var fs = require('fs');
+
+require.extensions['.css'] = function (module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8');
+};
+
+var inputCSS = require("./test/input.css");
+var outputCSS = require("./test/output.css");
+
+function run(t, input, output, opts) {
+  return postcss([ flexbox(opts) ]).process(input)
+    .then(function(result) {
+        t.same(result.css, output);
+        t.same(result.warnings().length, 0);
+    });
+}
+
+test('converts box and box-item to flexbox props and values', function(t) {
+  return run(t, inputCSS, outputCSS, { });
+});
